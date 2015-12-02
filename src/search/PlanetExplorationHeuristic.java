ddf08cd20fbed2075cee99222c667d74ba5b284a
arnis <arni@tmsoftware.is>
2015-12-02 14:40:02 +0000

    Practical 3 heuristic arrives

--- src/search/PlanetExplorationHeuristic.java
+++ src/search/PlanetExplorationHeuristic.java
@@ -6,8 +6,18 @@ public class PlanetExplorationHeuristic implements NodeFunction {
     @Override
     public int f(Node n) {
         Planet planet = (Planet) n.state;
-           //TODO 'arni
+        int accessiblePlaces = 0;
+        int width = planet.width();
+        int height = planet.height();
+        for(int x = 0; x < width; x++){
+            for(int y = 0; y< height; y++){
+                if(planet.isAccessible(x,y)){
+                    accessiblePlaces += 1;
+                }
+            }
+        }
 
-        return planet.getVisitedCoordinates().size() - planet.getBatteryLife();
+
+        return accessiblePlaces - planet.getVisitedCoordinates().size();
     }
 }
